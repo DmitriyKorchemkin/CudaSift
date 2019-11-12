2cd69b6fa8e26277e56227987b31914379faf9b1
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2019-11-12 15:22:21 +0300

    Reorganize CMakeLists

--- cudasift/src/cudaSiftH.cu
+++ cudasift/src/cudaSiftH.cu
@@ -7,15 +7,14 @@
 #include <cmath>
 #include <iostream>
 #include <algorithm>
-#include "cudautils.h"
+#include "cudasift/cudautils.h"
 
-#include "cudaImage.h"
-#include "cudaSift.h"
-#include "cudaSiftD.h"
-#include "cudaSiftH.h"
+#include "cudasift/cudaImage.h"
+#include "cudasift/cudaSift.h"
+#include "cudasift/cudaSiftD.h"
+#include "cudasift/cudaSiftH.h"
 
 #include "cudaSiftD.cu"
-#define VERBOSE
 
 void InitCuda(int devNum)
 {
